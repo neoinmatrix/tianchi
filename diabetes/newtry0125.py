93f113c84f5c5fe1d16b07f2ebac5e4912714e3e
neoinubuntu <neolinjian@gmail.com>
2018-01-25 13:12:50 +0800

    20180125

--- diabetes/newtry0125.py
+++ diabetes/newtry0125.py
@@ -39,17 +39,24 @@ def make_feat(train,test):
 train_feat,test_feat = make_feat(train,test)
 
 arr=[u'血糖',
-# u'中性粒细胞%',
-# u'淋巴细胞%',
-# u'单核细胞%',
-# u'嗜酸细胞%',
-# u'嗜碱细胞%',
-# u'乙肝表面抗原',
-# u'乙肝表面抗体',
-# u'乙肝e抗原',
-# u'乙肝e抗体',
-# u'乙肝核心抗体',
+# u'总胆固醇',
+u'高密度脂蛋白胆固醇',
+u'低密度脂蛋白胆固醇',
+u'中性粒细胞%',
+u'淋巴细胞%',
+u'单核细胞%',
+u'嗜酸细胞%',
+u'嗜碱细胞%',
+u'乙肝表面抗原',
+u'乙肝表面抗体',
+u'乙肝e抗原',
+u'乙肝e抗体',
+u'乙肝核心抗体',
 ]
+train_feat[u'血红蛋白']=train_feat[u'血红蛋白']*train_feat[u'红细胞计数']
+test_feat[u'血红蛋白']=test_feat[u'血红蛋白']*test_feat[u'红细胞计数']
+
+
 predictors = [f for f in test_feat.columns if f not in arr]
 
 
@@ -64,9 +71,9 @@ params = {
     'boosting_type': 'gbdt',
     'objective': 'regression',
     'metric': 'mse',
-    'sub_feature': 0.7,
+    # 'sub_feature': 0.7,
     'num_leaves': 60,
-    'colsample_bytree': 0.7,
+    # 'colsample_bytree': 0.7,
     'feature_fraction': 0.7,
     'min_data': 100,
     'min_hessian': 1,
@@ -79,12 +86,12 @@ test_preds = np.zeros((test_feat.shape[0], 5))
 # kf = KFold(len(train_feat), n_folds = 5, shuffle=True, random_state=520)
 kf = KFold(n_splits=5, shuffle=True,random_state=520)
 for i, (train_index, test_index) in enumerate(kf.split(train_feat.index)):
+    # if i==0:
+    #     continue
     print(u'第{}次训练...'.format(i))
     train_feat1 = train_feat.iloc[train_index]
     train_feat2 = train_feat.iloc[test_index]
-    # print train_feat1.head() ,categorical_feature=[u'性别']
-    lgb_train1 = lgb.Dataset(train_feat1[predictors], train_feat1[u'血糖'],feature_name=predictors, categorical_feature=[u'性别'])
-    # lgb_train1 = lgb.Dataset(train_feat1[predictors], train_feat1[u'血糖'])
+    lgb_train1 = lgb.Dataset(train_feat1[predictors], train_feat1[u'血糖'])
     lgb_train2 = lgb.Dataset(train_feat2[predictors], train_feat2[u'血糖'])
     gbm = lgb.train(params,
                     lgb_train1,
@@ -92,15 +99,29 @@ for i, (train_index, test_index) in enumerate(kf.split(train_feat.index)):
                     valid_sets=lgb_train2,
                     verbose_eval=100,
                     feval=evalerror,
-                    early_stopping_rounds=100)
-    feat_imp = pd.Series(gbm.feature_importance(), index=predictors).sort_values(ascending=False)
+                    early_stopping_rounds=50)
+    # if i==0:
+    #     continue
+    # feat_imp = pd.Series(gbm.feature_importance(), index=predictors).sort_values(ascending=False)
     # print feat_imp
+    # exit()
+    # if i==0:
+    #     train_preds[test_index] +=train_feat.loc[test_index,u'血糖']
+    # else:
     train_preds[test_index] += gbm.predict(train_feat2[predictors])
+
     test_preds[:,i] = gbm.predict(test_feat[predictors])
 
 print(u'线下得分：{}'.format(mean_squared_error(train_feat[u'血糖'],train_preds)*0.5))
 print(u'CV训练用时{}秒'.format(time.time() - t0))
-
+# print test_preds
+ 
+# np.savetxt("./a.txt",test_preds,fmt='%.4f')
+print test_preds.std()
+test_preds=np.delete(test_preds,0,axis=1)
+print test_preds.std()
+# np.savetxt("./b.txt",test_preds,fmt='%.4f')
+# print test_preds
 submission = pd.DataFrame({'pred':test_preds.mean(axis=1)})
 submission.to_csv(r'result{}.csv'.format(datetime.datetime.now().strftime('%Y%m%d_%H%M%S')),header=None,
                   index=False, float_format='%.4f')
